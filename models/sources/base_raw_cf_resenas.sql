0945df75f5f978f44e4acb1cf27b33ca09e9bebf
chekos <sergio@cimarron.io>
2024-03-07 16:58:39 -0800

    agrega pruebas

--- models/sources/base_raw_cf_resenas.sql
+++ models/sources/base_raw_cf_resenas.sql
@@ -1,16 +1,28 @@
-with source as (
-      select * from {{ source('raw', 'cf_resenas') }}
+WITH source AS (
+    SELECT
+        *
+    FROM
+        {{ source(
+            'raw',
+            'cf_resenas'
+        ) }}
 ),
-renamed as (
-    select
+renamed AS (
+    SELECT
         {{ adapter.quote("FEEDBACK_ID") }},
         {{ adapter.quote("COURSE_ID") }},
         {{ adapter.quote("USER_ID") }},
         {{ adapter.quote("RATING") }},
         {{ adapter.quote("COMMENT") }},
         {{ adapter.quote("CREATED_AT") }}
-
-    from source
+    FROM
+        source
 )
-select * from renamed
-  
+SELECT
+    *
+FROM
+    renamed
+WHERE
+    rating >= 1
+    AND rating <= 5
+    AND created_at IS NOT NULL
