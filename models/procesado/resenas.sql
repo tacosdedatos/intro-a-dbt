908298844464622c3d6d97c2254c5d2e02f179bf
chekos <sergio@cimarron.io>
2024-03-07 17:24:27 -0800

    agrega incremental model'

--- models/procesado/resenas.sql
+++ models/procesado/resenas.sql
@@ -1,4 +1,28 @@
-with base as (
-    select * from {{ ref("base_raw_cf_resenas")}}
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'feedback_id'
+) }}
+
+WITH base AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref("base_raw_cf_resenas") }}
 )
-select * from base 
+SELECT
+    *
+FROM
+    base
+
+{% if is_incremental() %}
+-- this filter will only be applied on an incremental run
+-- (uses >= to include records arriving later on the same day as the last run of this model)
+WHERE
+    created_at >= (
+        SELECT
+            MAX(created_at)
+        FROM
+            {{ this }}
+    )
+{% endif %}
